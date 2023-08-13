7df9606d396e4f682e7107048edef05758e48c02
Oyefule <drelanorgent@gmail.com>
2023-08-13 16:23:19 +0100

    Error handling and Authentication init

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
+process.on('uncaughtException', err => {
+  console.log(err.name, err.message);
+  process.exit(1);
+});
+
 dotenv.config({ path: './config.env' });
 
 const app = require('./app');
@@ -20,9 +25,15 @@ mongoose
   .then(() => console.log('DB connection successful'));
 
 const port = process.env.PORT;
-// eslint-disable-next-line spaced-comment
 
 //Initialize server
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`App running on port ${port}...`);
+});
+
+process.on('unhandledRejection', err => {
+  console.log(err.name, err.message);
+  server.close(() => {
+    process.exit(1);
+  });
 });
